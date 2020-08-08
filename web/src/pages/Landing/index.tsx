a6f2fe655da34069bb18b7895416ddae29b5f55b
deborahbaltasar <deborahbnogueira.dn@gmail.com>
2020-08-08 13:08:17 -0300

    backend connection

--- web/src/pages/Landing/index.tsx
+++ web/src/pages/Landing/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import { Link } from 'react-router-dom';
 
 import logoImg from '../../assets/images/logo.svg';
@@ -7,9 +7,20 @@ import studyIcon from '../../assets/images/icons/study.svg';
 import giveClassesIcons from '../../assets/images/icons/give-classes.svg';
 import purpleHeartIcon from '../../assets/images/icons/purple-heart.svg';
 
+import API from '../../services/api';
+
 import './styles.css';
 
 function Landing() {
+    const [totalConnections, setTotalConnections] = useState(0);
+
+    useEffect(() => {
+        API.get('connections').then(res => {
+            const { total } = res.data;
+
+            setTotalConnections(total);
+        })
+    }, [])
     return (
        <div id="page-landing">
            <div id="page-landing-content" className="container">
@@ -37,7 +48,7 @@ function Landing() {
                </div>
 
                <span className="total-connections">
-                   Total de 200 conexões já realizadas
+                   Total de {totalConnections} conexões já realizadas
                    <img src={purpleHeartIcon} alt="Coração roxo" />
                </span>
            </div>
--- web/src/pages/TeacherList/index.tsx
+++ web/src/pages/TeacherList/index.tsx
@@ -1,35 +1,94 @@
-import React from 'react';
+import React, {useState, useEffect, FormEvent} from 'react';
 import { Link } from 'react-router-dom';
 
 import PageHeader from '../../components/PageHeader';
-import TeacherItem from '../../components/TeacherItem';
+import TeacherItem, {Teacher} from '../../components/TeacherItem';
+import Input from '../../components/Input';
+import Select from '../../components/Select';
+
+import API from '../../services/api';
 
 import './styles.css';
 
 function TeacherList() {
+    const [teachers, setTeachers] = useState([]);
+
+    const [ subject, setSubject] = useState('');
+    const [ week_day , setWeek_day] = useState('');
+    const [ time, setTime] = useState('');
+
+    async function searchTeachers(e: FormEvent) {
+        e.preventDefault();
+
+        const res = await API.get('classes', {
+            params: {
+                subject,
+                week_day,
+                time,
+            }
+        })
+
+        setTeachers(res.data)
+    }
+
     return (
         <div id="page-teacher-list" className="container">
             <PageHeader title="Esses são os proffys disponíveis.">
-                <form id="search-teachers">
-                    <div className="input-block">
-                        <label htmlFor="subject">Matéria</label>
-                        <input type="text" id="subject"/>
-                    </div>
+                <form id="search-teachers" onSubmit={searchTeachers}>
+                    <Select 
+                        name="subject"
+                        value={subject}
+                        onChange={(e) => {setSubject(e.target.value)}} 
+                        label="Matéria"
+                        options={[
+                            { value: 'Artes', label: 'Artes' },
+                            { value: 'Biologia', label: 'Biologia' },
+                            { value: 'Física', label: 'Física' },
+                            { value: 'Geografia', label: 'Geografia' },
+                            { value: 'História', label: 'História' },
+                            { value: 'Inglês', label: 'Inglês' },
+                            { value: 'Matemática', label: 'Matemática' },
+                            { value: 'Português', label: 'Português' },
+                            { value: 'Química', label: 'Química' },
 
-                    <div className="input-block">
-                        <label htmlFor="week_day">Dia da semana</label>
-                        <input type="text" id="week_day"/>
-                    </div>
+                        ]}
+                    />
 
-                    <div className="input-block">
-                        <label htmlFor="time">Hora</label>
-                        <input type="text" id="time"/>
-                    </div>
+                    <Select 
+                        name="week_day" 
+                        value={week_day} 
+                        onChange={(e) => {setWeek_day(e.target.value)}}
+                        label="Dia da semana"
+                        options={[
+                            { value: '0', label: 'Domingo' },
+                            { value: '1', label: 'Segunda-feira' },
+                            { value: '2', label: 'Terça-feira' },
+                            { value: '3', label: 'Quarta-feira' },
+                            { value: '4', label: 'Quinta-feira' },
+                            { value: '5', label: 'Sexta-feira' },
+                            { value: '6', label: 'Sábado' },
+
+                        ]}
+                    />
+
+
+                    <Input 
+                        type="time" 
+                        name="time" 
+                        label="Hora" 
+                        value={time}
+                        onChange={(e) => {setTime(e.target.value)}} 
+                    />
+
+                    <button type="submit">Buscar</button>
+
                 </form>
             </PageHeader>
 
             <main>
-                <TeacherItem />
+                {teachers.map((teacher: Teacher) => (
+                    <TeacherItem key={teacher.id} teacher={teacher}/>
+                ))}
             </main>
         </div>
     );
