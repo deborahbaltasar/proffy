39f37e9851962b3bc9c2700f8da392aee05a5df0
deborahbaltasar <deborahbnogueira.dn@gmail.com>
2020-08-08 13:06:24 -0300

    create components

--- web/src/components/TeacherItem/index.tsx
+++ web/src/components/TeacherItem/index.tsx
@@ -3,33 +3,55 @@ import React from 'react';
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
 
 import './styles.css';
+import API from '../../services/api';
 
-const TeacherItem = () => {
+export interface Teacher{
+    avatar: string,
+    bio: string,
+    cost: number,
+    id: number,
+    name: string,
+    subject: string,
+    whatsapp: string
+}
+
+ interface TeacherItemProps {
+    teacher: Teacher
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({teacher}) => {
+    
+    function createNewConnection() {
+        API.post('connections', {
+            user_id: teacher.id,
+        })
+    }
+
     return (
         <article className="teacher-item">
             <header>
-                <img src="https://instagram.ffor8-2.fna.fbcdn.net/v/t51.2885-19/s150x150/13721051_313697058973568_1298614962_a.jpg?_nc_ht=instagram.ffor8-2.fna.fbcdn.net&_nc_ohc=vtoNB03NAgIAX_4FqTe&oh=6f0a1588ccf4070aeeb5d11ee4d6c2f2&oe=5F539119" alt=""/>
+                <img src={teacher.avatar} alt={teacher.name}/>
                 <div>
-                    <strong>Sarah Baltasar</strong>
-                    <span>Química</span>
+                    <strong>{teacher.name}</strong>
+                    <span>{teacher.subject}</span>
                 </div>
             </header>
 
-            <p>
-                Entusiasta das melhores tecnologias de química avançada
-                <br /><br />
-                Apaixonada por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões
-            </p>
+            <p>{teacher.bio}</p>
 
             <footer>
                 <p>
                     Preço/Hora 
-                    <strong>R$ 80,00</strong>
+                    <strong>{teacher.cost}</strong>
                 </p>
-                <button type="button">
+                <a
+                    target="_blank"
+                    onClick={createNewConnection} 
+                    href={`https://wa.me/${teacher.whatsapp}`}
+                >
                     <img src={whatsappIcon} alt="Whatsapp"/>
                     Entrar em contato
-                </button>
+                </a>
             </footer>
         </article>
     );
